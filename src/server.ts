d86553496dfe3f5d3aec32e8c28b5d7a46297577
Mehebul02 <mehebul2123@gmail.com>
2024-11-16 21:18:20 +0600

    config the app.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
+import mongoose from "mongoose";
 import config from "./app/config";
-
-const mongoose = require('mongoose');
+import app from "./app";
 
 async function main() {
-    await mongoose.connect(config.database_url);
-
-
+    try {
+        await mongoose.connect(config.database_url as string);
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 
-app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`)
-})
