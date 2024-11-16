37b5432af7eb1739d50072f629ce729fe199412e
Mehebul02 <mehebul2123@gmail.com>
2024-11-16 22:34:47 +0600

    enverment setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app:Application = express()
-const port = 3000
 
 // parser 
 
@@ -9,7 +8,8 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+    const a= 44
+  res.send(a)
 })
 
 export default app;
