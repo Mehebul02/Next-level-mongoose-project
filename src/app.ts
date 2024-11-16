387f8a48439e59604abf6ee7fac9b97f8c9df15e
Mehebul02 <mehebul2123@gmail.com>
2024-11-16 23:23:05 +0600

    setup the prettier

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-const app:Application = express()
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
-// parser 
+// parser
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req:Request, res:Response) => {
-    const a= 44
-  res.send(a)
-})
+app.get("/", (req: Request, res: Response) => {
+  const a = 44;
+
+  res.send(a);
+});
 
 export default app;
-
